13ddce6633394ae168ee2e869a1260e6d3b8c55a
Varun jha <jhavarun999@gmail.com>
2023-04-22 12:42:52 +0530

    first commit

---
+++ src/Api.js
@@ -0,0 +1,16 @@
+export const GeoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': "7175eabe70msh449fcf217dd1d9ep1958dcjsn422770a6019b",
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+
+
+export const WEATHER_API_KEY = "a446ba80042d00543b93383dfac73e7e"
